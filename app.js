d1682d9bc49e3e5041c2de96873b0bfbfcf7744f
Leon <t9276886@gmail.com>
2019-12-19 20:57:10 +0100

    Silly mistake fixed "const PORT = PORT = ..."

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 //const socketio = require('socket.io')
 
-const PORT = PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
 const server = express()
 
