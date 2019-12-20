08bef79f28ca2984e9b0629abd6c6b538f0746fe
Leon <t9276886@gmail.com>
2019-12-20 18:07:19 +0100

    Video Chat? More like Video Chad ;]

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ const server = express()
     .use((req, res) => res.sendFile(`${__dirname}/webcam.html`))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-// const io = socketio(server);
+const io = socketio(server);
 
-// io.on('connection', socket => {
-//     console.log('User connected')
+io.on('connection', socket => {
+    console.log('User connected')
 
+    socket.on('frame', data => {
+        //console.log(`Received frame from: ${data.uid}`)
+        socket.broadcast.emit('frameRecv', data)
+    })
 
-
-//     socket.on('disconnect', () => {
-//         console.log('User Disconnected')
-//     })
-// })
+    socket.on('disconnect', () => {
+        console.log('User Disconnected')
+    })
+})
